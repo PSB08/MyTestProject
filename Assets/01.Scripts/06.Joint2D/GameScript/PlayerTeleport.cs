145c83f159b387475d553790f473df12edfb3cc2
PSB08 <Parkseongbin08@gmail.com>
2024-11-07 00:29:12 +0900

    ConfinerScript
    텔레포트 하면서 confiner에 collider 바꿔주도록 만듦

--- Assets/01.Scripts/06.Joint2D/GameScript/PlayerTeleport.cs
+++ Assets/01.Scripts/06.Joint2D/GameScript/PlayerTeleport.cs
@@ -1,3 +1,5 @@
+using System;
+using Cinemachine;
 using UnityEngine;
 
 namespace _01.Scripts._06.Joint2D.GameScript
@@ -5,18 +7,38 @@ namespace _01.Scripts._06.Joint2D.GameScript
     public class PlayerTeleport : MonoBehaviour
     {
         [SerializeField] private GameObject player;
+        [SerializeField] private GameObject target;
+        [SerializeField] private CinemachineVirtualCamera cam;
+        [SerializeField] private CinemachineConfiner confiner;
+        [SerializeField] private PolygonCollider2D originCollider;
+        [SerializeField] private PolygonCollider2D nextCollider;
+
+        private void Awake()
+        {
+            cam = FindAnyObjectByType<CinemachineVirtualCamera>();
+            confiner = cam.GetComponent<CinemachineConfiner>();
+        }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.CompareTag("PlayerCollider"))
             {
                 ActionPlayer();
+                ChangeConfiner();
             }
         }
         private void ActionPlayer()
         {
-            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 5,
-                player.transform.position.z);
+            player.transform.position = new Vector3(target.transform.position.x, target.transform.position.y,
+                target.transform.position.z);
         }
+
+        private void ChangeConfiner()
+        {
+            confiner.m_BoundingShape2D = nextCollider;
+        }
+
+
+
     }
 }
