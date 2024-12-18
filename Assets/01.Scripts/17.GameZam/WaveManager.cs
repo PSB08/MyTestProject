cbaf2972912ad84cde5e2bc79a0bf40e7a923f57
PSB08 <Parkseongbin08@gmail.com>
2024-12-18 12:43:08 +0900

    BombEnemy and DashEnemy

--- Assets/01.Scripts/17.GameZam/WaveManager.cs
+++ Assets/01.Scripts/17.GameZam/WaveManager.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WaveManager : MonoSingletone<WaveManager>
 {
+    [Header("EnemyPrefab")]
     [SerializeField] private GameObject meleeEnemyPrefab;
     [SerializeField] private GameObject rangedEnemyPrefab;
+    [SerializeField] private GameObject bombEnemyPrefab;
+    [SerializeField] private GameObject dashEnemyPrefab;
+    [Space(10)]
+    [Header("BossPrefab")]
     [SerializeField] private GameObject bossPrefab;
     [SerializeField] private GameObject bossPrefab2;
     [SerializeField] private GameObject bossPrefab3;
+    [Space(10)]
+    [Header("SpawnPoint")]
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private Transform bossSpawnpoint;
-
+    [Space(10)]
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI waveTxt;
+    [Space(10)]
+    [Header("List")]
     [SerializeField] private List<Transform> trans = new List<Transform>();
 
     private int currentWave = 0;
@@ -19,6 +31,7 @@ public class WaveManager : MonoSingletone<WaveManager>
 
     private void Start()
     {
+        waveTxt.text = "";
         StartWave();
     }
 
@@ -32,6 +45,7 @@ public class WaveManager : MonoSingletone<WaveManager>
         if (currentWave % 5 == 0)
         {
             spawnPoint = bossSpawnpoint;
+            waveTxt.text = $"BossWave";
             if (currentWave == 5)
             {
                 Instantiate(bossPrefab, spawnPoint.position, Quaternion.identity);
@@ -50,7 +64,29 @@ public class WaveManager : MonoSingletone<WaveManager>
         {
             for (int i = 0; i < numberOfEnemies; i++)
             {
-                GameObject enemyToSpawn = Random.Range(0, 2) == 0 ? meleeEnemyPrefab : rangedEnemyPrefab;
+                waveTxt.text = $"Wave : {currentWave}";
+
+                GameObject enemyToSpawn;
+                int enemyType = Random.Range(0, 4); 
+
+                switch (enemyType)
+                {
+                    case 0:
+                        enemyToSpawn = meleeEnemyPrefab; 
+                        break;
+                    case 1:
+                        enemyToSpawn = rangedEnemyPrefab; 
+                        break;
+                    case 2:
+                        enemyToSpawn = bombEnemyPrefab;
+                        break;
+                    case 3:
+                        enemyToSpawn = dashEnemyPrefab; 
+                        break;
+                    default:
+                        enemyToSpawn = meleeEnemyPrefab;
+                        break;
+                }
                 spawnPoint = trans[Random.Range(0, trans.Count)];
                 Instantiate(enemyToSpawn, spawnPoint.position, Quaternion.identity);
             }
